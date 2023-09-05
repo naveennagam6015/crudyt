5099bd67788525b76981fe42d0a4a830644a342e
unknown <naveennagam6015@gmail.com>
2023-09-05 13:32:53 +0530

    Title changed

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Naveen Nagam",
+    Age: "21"
+  },
+  {
+    id: "2",
+    Name: "Virat Kohli",
+    Age: "31"
+  },
+  {
+    id: "3",
+    Name: "Rohit Sharma",
+    Age: "33"
+  }
+];
+export default Employees;
